bf3d7a0a2e4e0f9fea4db26f36ca4794227c62e7
supratim-pal <45565566+supratim-pal@users.noreply.github.com>
2019-01-01 12:22:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,111 @@
 import os
-from flask import Flask
+import websockets
+import asyncio
+import json
+import time, osfrom flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+
+class HttpWSSProtocol(websockets.WebSocketServerProtocol):
+    rwebsocket = None
+    rddata = None
+    async def handler(self):
+        try:
+            request_line, headers = await websockets.http.read_message(self.reader)
+            method, path, version = request_line[:-2].decode().split(None, 2)
+            #websockets.accept()
+            print(headers)
+        except Exception as e:
+            print(e.args)
+            self.writer.close()
+            self.ws_server.unregister(self)
+
+            raise
+
+        # TODO: Check headers etc. to see if we are to upgrade to WS.
+        if path == '/ws':
+            # HACK: Put the read data back, to continue with normal WS handling.
+            self.reader.feed_data(bytes(request_line))
+            self.reader.feed_data(headers.as_bytes().replace(b'\n', b'\r\n'))
+
+            return await super(HttpWSSProtocol, self).handler()
+        else:
+            try:
+                return await self.http_handler(method, path, version)
+            except Exception as e:
+                print(e)
+            finally:
+
+                self.writer.close()
+                self.ws_server.unregister(self)
+
+
+    async def http_handler(self, method, path, version):
+        response = ''
+        try:
+
+            googleRequest = self.reader._buffer.decode('utf-8')
+            googleRequestJson = json.loads(googleRequest)
+
+            #{"location": "living", "state": "on", "device": "lights"}
+            if 'what' in googleRequestJson['queryResult']['queryText']:
+                ESPparameters = googleRequestJson['queryResult']['parameters']
+                ESPparameters['query'] = '?'
+            else:
+                ESPparameters = googleRequestJson['queryResult']['parameters']
+                ESPparameters['query'] = 'cmd'
+            # send command to ESP over websocket
+            print(ESPparameters)
+            if self.rwebsocket== None:
+                print("Device is not connected!")
+                return
+            await self.rwebsocket.send(json.dumps(ESPparameters))
+
+            #wait for response and send it back to API.ai as is
+            self.rddata = await self.rwebsocket.recv()
+            #{"speech": "It is working", "displayText": "It is working"}
+            print(self.rddata)
+            #state = json.loads(self.rddata)['state']
+            #self.rddata = '{"speech": "It is turned '+state+'", "displayText": "It is turned '+state+'"}'
+            self.rddata = '{"fulfillmentText": "It is turned on, yes"}'
+
+            response = '\r\n'.join([
+                'HTTP/1.1 200 OK',
+                'Content-Type: text/json',
+                '',
+                ''+self.rddata+'',
+            ])
+        except Exception as e:
+            print("here")
+            print(e)
+        self.writer.write(response.encode())
+
+def updateData(data):
+    HttpWSSProtocol.rddata = data
+
+async def ws_handler(websocket, path):
+    game_name = 'g1'
+    try:
+        HttpWSSProtocol.rwebsocket = websocket
+        await websocket.send(json.dumps({'event': 'OK'}))
+        data ='{"empty":"empty"}'
+        while True:
+            data = await websocket.recv()
+            print(data)
+            updateData(data)
+    except Exception as e:
+        print(e)
+    finally:
+        print("")
+
   
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.getenv('PORT', 5687))
+	start_server = websockets.serve(ws_handler, '', port, klass=HttpWSSProtocol)
+    # logger.info('Listening on port %d', port)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+    #app.run(host='0.0.0.0', port=port)
